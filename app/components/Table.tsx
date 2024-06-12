91716bad4ee4eb12c4b88e6d8d133f9570f8d9ed
houfio <github@houf.io>
2024-06-12 17:28:56 +0200

    Improve table stying

--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -3,15 +3,12 @@ import type { ReactNode } from 'react';
 
 import styles from './Table.module.scss';
 
-import { withPalette } from '~/utils/withPalette';
-
 type Props<T> = {
   data: T[],
   rowKey: keyof T,
   columns: {
     [K in keyof T]?: ColumnConfig<T, K>
-  },
-  palette?: string
+  }
 };
 
 type ColumnConfig<T, K extends keyof T> = {
@@ -19,14 +16,14 @@ type ColumnConfig<T, K extends keyof T> = {
   render: (row: T[K], data: T) => ReactNode
 };
 
-export function Table<T>({ data, rowKey, columns, palette = 'background' }: Props<T>) {
+export function Table<T>({ data, rowKey, columns }: Props<T>) {
   const columnArrays = Object.entries(columns).map(([key, value]) => ({
     key: key as keyof T & string,
     config: value as ColumnConfig<T, keyof T>
   }));
 
   return (
-    <table style={withPalette(palette)} className={styles.table}>
+    <table className={styles.table}>
       <thead className={styles.header}>
         <tr>
           {columnArrays.map(({ key, config }) => (
@@ -41,7 +38,7 @@ export function Table<T>({ data, rowKey, columns, palette = 'background' }: Prop
       </thead>
       <tbody>
         {data.map((row) => (
-          <tr key={String(row[rowKey])}>
+          <tr key={String(row[rowKey])} className={styles.row}>
             {columnArrays.map(({ key, config }) => (
               <td key={key} className={styles.body}>
                 {config.render(row[key], row)}
