186973768c74f5405ea1ab198c03f447838808fc
houfio <github@houf.io>
2024-11-18 21:09:45 +0100

    Update dependencies

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,10 @@
 import { type RouteConfig, index, layout, route } from '@react-router/dev/routes';
 
-export const routes: RouteConfig = [
+export default [
   layout('routes/_layout.tsx', [index('routes/_index.tsx')]),
   layout('routes/_auth/_layout.tsx', [
     route('/login', 'routes/_auth/login.tsx'),
     route('/register', 'routes/_auth/register.tsx'),
     route('/logout', 'routes/_auth/logout.ts')
   ])
-];
+] satisfies RouteConfig;
